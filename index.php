269fc4af68b5533eae83273714e002b39feaa678
SECT <sect@sect.me>
2022-05-20 03:57:06 +0900

    Bump version 1.2.0

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ Plugin Name: CFS - DateTime Add-on
 Plugin URI: https://github.com/sectsect/cfs-datetime
 Description: Add Powerful Datetimepicker field type for Custom Field Suite using flatpickr.js.
 Author: SECT INTERACTIVE AGENCY
-Version: 1.1.0
-Author URI: https://www.ilovesect.com/
+Version: 1.2.0
+Author URI: https://github.com/sectsect
 License: GPL2
 */
 
